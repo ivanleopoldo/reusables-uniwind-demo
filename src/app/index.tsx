6aa73a14a20d5f34945e227b0d13776804e19765
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-12-24 00:39:05 +0800

    setups uniwind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="justify-center items-center flex-1">
+      <Text className="text-6xl font-bold">Hello, World</Text>
     </View>
   );
 }
